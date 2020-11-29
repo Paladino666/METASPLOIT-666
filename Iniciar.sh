a7435336539c4d87501380b65c72ed872465595a
Paladino666 <75174943+Paladino666@users.noreply.github.com>
2020-11-28 23:41:09 -0300

    Update Iniciar.sh

--- Iniciar.sh
+++ Iniciar.sh
@@ -1,4 +1,55 @@
 #!/bin/bash
-
  clear
+
+setterm -foreground green -store
+
+clear
+
+figlet -f mono12 "METASPLOIT"
+
+termux-open-url https://www.youtube.com/channel/UCkvRIcijkQywJgOpoj-1diQ
+
+echo "CONTINIUAR[C]SAIR[S]"
+read PROCESSANDO
+
+if [ "$PROCESSANDO" = "C" ]
+  then
+  echo "O METASPLOIT ESTAR BAIXANDO"
+
+figlet -f mono12 "BAIXANDO"
+
+
+wget https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+
+clear
+
+setterm -foreground green -store
+
+clear
+
+curl -O https://raw.githubusercontent.com/th3unkn0n/extra/master/msfdb
+
+clear
+
+setterm -foreground green -store
+
+clear
+
+chmod 777 metasploit.sh
+
+./metasploit.sh
+
+fi
+
+if [ "$PROCESSANDO" = "S" ]
+  then
+  ^C
+
+setterm -foreground red -store
+
+clear
+
+figlet -f mono12 "SAINDO"
+fi
+
 
